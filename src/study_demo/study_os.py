48a62c689ea12c61d3c39e05b0a709601ddb5ee9
wjb675020495 <675020495@qq.com>
2020-04-27 00:55:28 +0800

    commit study os module

---
+++ src/study_demo/study_os.py
@@ -0,0 +1,25 @@
+import os
+
+
+def get_all_files():
+    cur_path = os.getcwd()
+    paths_dir = os.listdir(cur_path)
+    path_list = []
+    for path in paths_dir:
+        file_path = os.path.join(cur_path, path)
+        path_list.append(file_path)
+    return path_list
+
+
+def read_file(path):
+    with open(path, 'r') as all_file:
+        print(all_file.read())
+        files_content = all_file.read()
+        if files_content.find('add') != -1:
+            print(files_content)
+
+
+if __name__ == '__main__':
+    files_all = get_all_files()
+    for file in files_all:
+        read_file(file)
